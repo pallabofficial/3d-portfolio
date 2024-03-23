7fcac7d72793109e7bbb3093eb3a0b03f591fbc9
pallabofficial <officialpallabmondal000@gmail.com>
2024-03-23 11:08:56 +0530

    Starting files: created a react app through vite, installed tailwind and other packages

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {BrowserRouter} from 'react-router-dom';
+import {Navbar, Hero, About, Contact, Experience, Feedbacks, Tech, Works, StarsCanvas} from "./components";
+
+
+const App = () => {
+  return (
+    <div className=''> 
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
